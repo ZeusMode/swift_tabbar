f9f1191b12102e47e445fb764ffe7db249577b38
ZeusMode <cintra.wesley@gmail.com>
2016-10-23 21:53:27 -0200

    first commit

---
+++ TabBarExample/GreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GreenViewController.swift
+//  TabBarExample
+//
+//  Created by Wesley Cintra Nascimento on 20/10/16.
+//  Copyright © 2016 Wesley Cintra Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
